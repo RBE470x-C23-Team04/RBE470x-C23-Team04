72f72adf5e729279d708afb54ba3bf0b789840e8
Alex Brattstrom <abrattstrom@wpi.edu>
2023-01-29 17:53:49 -0500

    stash

--- team04/testcharacter.py
+++ team04/testcharacter.py
@@ -4,9 +4,49 @@ sys.path.insert(0, '../bomberman')
 # Import necessary stuff
 from entity import CharacterEntity
 from colorama import Fore, Back
+from priority_queue import PriorityQueue
 
 class TestCharacter(CharacterEntity):
 
+    
+    
+    def findGoal(self, wrld):
+        '''
+        Run once to find world exit.
+        '''
+        world_width = wrld.width()
+        world_height = wrld.height()
+        for x in range(0,world_width):
+            for y in range(0,world_height):
+                if wrld.exit_at(x,y):
+                    goal = (x,y)
+                    foundGoal = True
+        return goal
+
+
+    def a_star(self, mapdata, start, goal):
+        '''
+        '''
+        
+        frontier = PriorityQueue()
+        frontier.put(start, 0)
+        came_from = {}
+        cost_so_far = {}
+        came_from[start] = None
+        cost_so_far[start] = 0
+        
+        
+        
+    
     def do(self, wrld):
-        # Your code here
+        
+        if not foundGoal:
+            goal = self.findGoal(wrld)
+            print("Goal " + goal)
+        
+        dx, dy = 0, 0 # starting pos
+        
+        
+            
+           
         pass
