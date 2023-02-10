5b77807b1e2363ab46b4268ffdca815445a3f716
Alex Brattstrom <abrattstrom@wpi.edu>
2023-02-09 23:08:09 -0500

    Added print output for character end events (eg: me killed by self)

--- team04/project1/variant5.py
+++ team04/project1/variant5.py
@@ -17,126 +17,140 @@ from testcharacter import TestCharacter
 
 # Create the game
 #123 673 938 293 193
-# random.seed(3) # TODO Change this if you want different random choices
-# g = Game.fromfile('map.txt')
-# g.add_monster(StupidMonster("stupid", # name
-#                             "S",      # avatar
-#                             3, 5,     # position
-# ))
-# g.add_monster(SelfPreservingMonster("aggressive", # name
-#                                     "A",          # avatar
-#                                     3, 13,        # position
-#                                     1             # detection range
-# ))
+random.seed(3) # TODO Change this if you want different random choices
+g = Game.fromfile('map.txt')
+g.add_monster(StupidMonster("stupid", # name
+                            "S",      # avatar
+                            3, 5,     # position
+))
+g.add_monster(SelfPreservingMonster("aggressive", # name
+                                    "A",          # avatar
+                                    3, 13,        # position
+                                    1             # detection range
+))
 
-# # TODO Add your character
-# g.add_character(TestCharacter("me", # name
-#                               "C",  # avatar
-#                               0, 0  # position
-# ))
+# TODO Add your character
+g.add_character(TestCharacter("me", # name
+                              "C",  # avatar
+                              0, 0  # position
+))
 
-# # Run!
-# g.go(0)
+# Run!
+g.go(0)
 
 
 
 ### Uncomment below to run the variant with various seeds.
 
-# Set inputs:
-seed_range_lower = 1
-seed_range_higher = 1000
+# # Set inputs:
+# seed_range_lower = 1
+# seed_range_higher = 1000
 
 
-success_cnt = 0
-seed_outcome = {}
-seed_count = seed_range_higher - seed_range_lower + 1
+# success_cnt = 0
+# killed_itself = 0
+# killed_by_stupid = 0
+# killed_by_aggressive = 0
+# seed_killed = 0 #temp variable for killed by stupid
+# seed_outcome = {}
+# seed_count = seed_range_higher - seed_range_lower + 1
 
-# Disable
-def blockPrint():
-    sys.stdout = open(os.devnull, 'w')
+# # Disable
+# def blockPrint():
+#     sys.stdout = open(os.devnull, 'w')
 
-# Restore
-def enablePrint():
-    sys.stdout = sys.__stdout__
+# # Restore
+# def enablePrint():
+#     sys.stdout = sys.__stdout__
     
+# # enablePrint()
+# blockPrint()
+
+# for i in range(seed_range_lower,seed_range_higher+1):
+#     random.seed(i)
+
+#     print("Seed #: " + str(i))
+    
+#     g = Game.fromfile('map.txt')
+#     g.add_monster(StupidMonster("stupid", # name
+#                                 "S",      # avatar
+#                                 3, 5,     # position
+#     ))
+#     g.add_monster(SelfPreservingMonster("aggressive", # name
+#                                         "A",          # avatar
+#                                         3, 13,        # position
+#                                         1             # detection range
+#     ))
+
+#     # TODO Add your character
+#     g.add_character(TestCharacter("me", # name
+#                                 "C",  # avatar
+#                                 0, 0  # position
+#     ))
+    
+#     # print("Seed " + str(i))
+    
+#     # blockPrint()
+#     g.go(1)
+#     # enablePrint()
+    
+#     events = g.events
+#     print("Amount of events " + str(len(events)))
+    
+#     for event in range(0,len(events)): # go through all events. Search for character, me, found the exit
+#         # print("event " + str(g.events[0]))
+#         if (str(g.events[event]) == "me found the exit"):
+#             success_cnt = success_cnt + 1   # increase success counter
+#             print("Success Count: " + str(success_cnt))
+#         seed_outcome[event+i] = str(g.events[event]) # add event to seed_outcome
+#             # print("Seed " + str(i) + str(g.events[0]))
+#         print("Event Number: " + str(event+1) + " - " + str(events[event]))
+#         # print("Event " + str(seed_outcome[event]))
+#         if (str(g.events[event]) == "me killed itself"):
+#             killed_itself = killed_itself + 1
+#         if (str(g.events[event]) == "me was killed by stupid"):
+#             killed_by_stupid = killed_by_stupid + 1
+#             seed_killed = i
+#         if (str(g.events[event]) == "me was killed by aggressive"):
+#             killed_by_aggressive = killed_by_aggressive + 1
+    
+    
+#     print("Seed Outcome(s): ", end='')
+#     seed_events = []
+#     for n in range(0,len(g.events)):
+#         # print(str(g.events[n]), sep=', ', end='', flush=True)
+#         # print(str(g.events[n]), sep=', ', end='')
+#         seed_events.append(str(events[n]))
+#     print(str(seed_events), sep=', ', end='', flush=True)
+    
+#     seed_outcome[i] = seed_events # append list of outcomes for a seed to the seed outcome's list
+
+#     # print(str(g.events), sep=', ', end='', flush=True)
+#     # print("Seed Outcome: " + str(seed_outcome[i]))
+#     print("")
+#     print("")
+    
+    
+    
+#     enablePrint()
+    
+#     # print("Mod check" + str(5%2))
+    
+#     # percent = int(((i*100)/seed_count))
+#     percent = ((i*100)/seed_count)
+#     # if ((percent % 10) == 0):
+#     print("Progress: " + str(percent) + "%")
+    
+#     blockPrint()
+    
+    
+# for ii in range(seed_range_lower,seed_range_higher+1):
+#     print("Seed " + str(ii) + " " + str(seed_outcome[ii]))
+
 # enablePrint()
-blockPrint()
-
-for i in range(seed_range_lower,seed_range_higher+1):
-    random.seed(i)
-
-    print("Seed #: " + str(i))
-    
-    g = Game.fromfile('map.txt')
-    g.add_monster(StupidMonster("stupid", # name
-                                "S",      # avatar
-                                3, 5,     # position
-    ))
-    g.add_monster(SelfPreservingMonster("aggressive", # name
-                                        "A",          # avatar
-                                        3, 13,        # position
-                                        1             # detection range
-    ))
-
-    # TODO Add your character
-    g.add_character(TestCharacter("me", # name
-                                "C",  # avatar
-                                0, 0  # position
-    ))
-    
-    # print("Seed " + str(i))
-    
-    # blockPrint()
-    g.go(1)
-    # enablePrint()
-    
-    events = g.events
-    print("Amount of events " + str(len(events)))
-    
-    for event in range(0,len(events)): # go through all events. Search for character, me, found the exit
-        # print("event " + str(g.events[0]))
-        if (str(g.events[event]) == "me found the exit"):
-            success_cnt = success_cnt + 1   # increase success counter
-            print("Success Count: " + str(success_cnt))
-        seed_outcome[event+i] = str(g.events[event]) # add event to seed_outcome
-            # print("Seed " + str(i) + str(g.events[0]))
-        print("Event Number: " + str(event+1) + " - " + str(events[event]))
-        # print("Event " + str(seed_outcome[event]))
-    
-    
-    print("Seed Outcome(s): ", end='')
-    seed_events = []
-    for n in range(0,len(g.events)):
-        # print(str(g.events[n]), sep=', ', end='', flush=True)
-        # print(str(g.events[n]), sep=', ', end='')
-        seed_events.append(str(events[n]))
-    print(str(seed_events), sep=', ', end='', flush=True)
-    
-    seed_outcome[i] = seed_events # append list of outcomes for a seed to the seed outcome's list
-
-    # print(str(g.events), sep=', ', end='', flush=True)
-    # print("Seed Outcome: " + str(seed_outcome[i]))
-    print("")
-    print("")
-    
-    
-    
-    enablePrint()
-    
-    # print("Mod check" + str(5%2))
-    
-    # percent = int(((i*100)/seed_count))
-    percent = ((i*100)/seed_count)
-    # if ((percent % 10) == 0):
-    print("Progress: " + str(percent) + "%")
-    
-    blockPrint()
-    
-    
-for ii in range(seed_range_lower,seed_range_higher+1):
-    print("Seed " + str(ii) + " " + str(seed_outcome[ii]))
-
-enablePrint()
-print("")
-print("Out of " + str(seed_count) + " seeds. Me found the exit " + str(success_cnt) + " time(s).")
+# print("")
+# print("Out of " + str(seed_count) + " seeds. Me found the exit " + str(success_cnt) + " time(s).")
+# print("Me killed by self "+ str(killed_itself) + " time(s).")
+# print("Me killed by stupid " + str(killed_by_stupid) + " time(s). Seed: " + str(seed_killed))
+# print("Me killed by aggressive " + str(killed_by_aggressive) + " time(s).")
 
